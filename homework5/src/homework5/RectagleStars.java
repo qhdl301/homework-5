2be48350feb3a0199404f3fb769a02ad35e7f196
Young soo Kim <qhdl301@gmail.com>
2016-10-26 11:36:09 +0900

    정사각형 크기 입력 문제  완료!!

--- homework5/src/homework5/RectagleStars.java
+++ homework5/src/homework5/RectagleStars.java
@@ -1,10 +1,34 @@
 package homework5;
 import java.util.*;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
 public class RectagleStars {
-	
+	Scanner s=new Scanner(System.in);	
+    int num;
+    String queqe;
 	void input(){
-		
+        
+		do{
+	    this.queqe="";
+		System.out.println("정사각형 크기입력: ");
+		if(s.hasNextInt()){
+		num=s.nextInt();
+		if(num!=0){
+	    	
+            for(int j=1; j<=5; j++){
+			for(int i=1; i<=num; i++)
+	    	{
+	    		System.out.print("*");
+	    	}
+			System.out.println();
+            }
+	    }
+		else print();
+		}else {
+			this.queqe = s.next();
+		}
+	}while(!queqe.equals("q"));
 	}
-
-
+	void print(){
+		System.out.println("잘못입력하셨습니다.");
+	}
 }

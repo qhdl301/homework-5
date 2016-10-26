842df5e272071afffa374a2a6ed30643f85996ec
Young soo Kim <qhdl301@gmail.com>
2016-10-27 08:31:18 +0900

    정삼각형 만들기 완료!!

--- homework5/src/homework5/IsoscelesTriangleStars.java
+++ homework5/src/homework5/IsoscelesTriangleStars.java
@@ -2,8 +2,37 @@ package homework5;
 import java.util.*;
 
 public class IsoscelesTriangleStars {
+Scanner s= new Scanner(System.in);
+int num;
+int num2;
+String queqe;
 
 	void input(){
-		
+		do{
+     	   this.queqe=" ";
+     	   System.out.println("이등변삼각형 높이 입력(q입력하면 리스트로): ");
+     	   if(s.hasNextInt()){
+     		   num=s.nextInt();
+     		   if(num!=0){
+     			   for(int j=0; j<num; j++){
+     			   for(int i=0; i<num-j; i++){
+     					   System.out.print(" ");
+     				   }
+     				for(int a=0; a<=j*2; a++){   
+     				System.out.print("*");   
+     			   }
+     			   System.out.println();
+     			   }
+     		   }
+     		   else print();
+     	   }else{
+     		   this.queqe=s.next();
+     	   }
+        }while(!queqe.equals("q"));
+        
 	}
-}
+	void print(){
+		System.out.println("잘못입력하셨습니다.");
+	}		
+	}
+
